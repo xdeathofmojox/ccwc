18a6271aad372b51422007e13561c871ecd4f710
xdeathofmojox <xdeathofmojox@gmail.com>
2026-04-11 15:46:17 -0400

    Remove prints

--- zig/cc-wc/src/main.zig
+++ zig/cc-wc/src/main.zig
@@ -21,11 +21,4 @@ pub fn main() !void {
             std.debug.print("Flag set: characters\n", .{});
         }
     }
-
-    std.debug.print("{s}\n", .{cc_wc_core.hello});
-    std.debug.print("Flag enum values:\n", .{});
-    std.debug.print("- bytes: {d}\n", .{cc_wc_core.Flag.bytes});
-    std.debug.print("- lines: {d}\n", .{cc_wc_core.Flag.lines});
-    std.debug.print("- words: {d}\n", .{cc_wc_core.Flag.words});
-    std.debug.print("- characters: {d}\n", .{cc_wc_core.Flag.characters});
 }
