3a7c935a65f8bb3dd30af6ed60edc358e261a597
xdeathofmojox <xdeathofmojox@gmail.com>
2026-04-11 13:31:18 -0400

    Reading args

--- zig/cc-wc/src/main.zig
+++ zig/cc-wc/src/main.zig
@@ -11,9 +11,15 @@ pub fn main() !void {
     var iter = try std.process.argsWithAllocator(allocator);
     defer iter.deinit();
 
+    var flags = std.AutoHashMap(cc_wc_core.Flag, void).init(allocator);
+    defer flags.deinit();
     // The first argument is usually the executable path
     while (iter.next()) |arg| {
         std.debug.print("Arg: {s}\n", .{arg});
+        if (std.mem.eql(u8, arg, "-c")) {
+            try flags.put(cc_wc_core.Flag.characters, {});
+            std.debug.print("Flag set: characters\n", .{});
+        }
     }
 
     std.debug.print("{s}\n", .{cc_wc_core.hello});
