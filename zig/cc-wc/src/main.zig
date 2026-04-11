4fa58de938219ea5753b2cac0dda695042ce40c4
xdeathofmojox <xdeathofmojox@gmail.com>
2026-04-11 13:03:52 -0400

    Arg parsing

--- zig/cc-wc/src/main.zig
+++ zig/cc-wc/src/main.zig
@@ -2,7 +2,20 @@ const std = @import("std");
 const cc_wc_core = @import("cc-wc-core");
 
 // TODO: Implement cc-wc executable in Zig
-pub fn main() void {
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    // Use an iterator for memory efficiency
+    var iter = try std.process.argsWithAllocator(allocator);
+    defer iter.deinit();
+
+    // The first argument is usually the executable path
+    while (iter.next()) |arg| {
+        std.debug.print("Arg: {s}\n", .{arg});
+    }
+
     std.debug.print("{s}\n", .{cc_wc_core.hello});
     std.debug.print("Flag enum values:\n", .{});
     std.debug.print("- bytes: {d}\n", .{cc_wc_core.Flag.bytes});
