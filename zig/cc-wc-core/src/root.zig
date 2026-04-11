052e342af96d5403db6e45ef608020fe57fdd7f0
xdeathofmojox <xdeathofmojox@gmail.com>
2026-04-11 12:52:28 -0400

    Start pulling from zig core lib

--- zig/cc-wc-core/src/root.zig
+++ zig/cc-wc-core/src/root.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const flag = @import("flag.zig");
 
 // TODO: Implement cc-wc-core library in Zig
 pub const hello = "Hello from cc-wc-core!";
+pub const Flag = flag.Flag;
--- zig/cc-wc/src/main.zig
+++ zig/cc-wc/src/main.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
-const hello = @import("cc-wc-core").hello;
+const cc_wc_core = @import("cc-wc-core");
 
 // TODO: Implement cc-wc executable in Zig
 pub fn main() void {
-    std.debug.print("{s}\n", .{hello});
+    std.debug.print("{s}\n", .{cc_wc_core.hello});
+    std.debug.print("Flag enum values:\n", .{});
+    std.debug.print("- bytes: {d}\n", .{cc_wc_core.Flag.bytes});
+    std.debug.print("- lines: {d}\n", .{cc_wc_core.Flag.lines});
+    std.debug.print("- words: {d}\n", .{cc_wc_core.Flag.words});
+    std.debug.print("- characters: {d}\n", .{cc_wc_core.Flag.characters});
 }
